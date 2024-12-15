e1fa7e58d90f66708915e294f331fc94feca59a4
Victor Meyo <victor.meyo@student.moringaschool.com>
2024-12-15 21:00:20 +0300

    First 2 fucntions complete. Third function pending

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+//Start of saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+//End of saturdayFun function
+
+//Start of mondayWork function
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+//End of mondayWork function
+
+//Start of wrapper function
